e70ca4155e3fc0c413f2b30a422ce6e41805c2b5
Emalcom <eng24002@byui.edu>
2024-10-30 18:14:48 +0300

    project finished

---
+++ project/scripts/script.js
@@ -0,0 +1,8 @@
+function toggleNav() {
+    const navLinks = document.getElementById("nav-links");
+    if (navLinks.style.display === "flex") {
+        navLinks.style.display = "none";
+    } else {
+        navLinks.style.display = "flex";
+    }
+}
